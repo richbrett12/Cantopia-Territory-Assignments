c3eedf3b54a4f72cfb06fe129c5b09bd17a94d67
Brett Rich <richbrett12@gmail.com>
2023-12-04 10:54:37 -0700

    Added Room Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import "./App.css";
+import * as React from "react";
 import TerritoryAssignment from "./components/TerritoryAssignment";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
+function Home() {
+  const rooms = ["Room1", "Room2", "Room3", "Room4"];
+  return (
+    <div>
+      <h1>Pages</h1>
+      <nav>
+        <ul>
+          {rooms.map((roomName) => {
+            return (
+              <li key={roomName}>
+                <Link to={`/${roomName}`}>{roomName}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <TerritoryAssignment />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:roomName" element={<TerritoryAssignment />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
